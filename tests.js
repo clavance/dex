261b165dae54c18d8d30aef1906b3ae065060fb5
Tom Walton <tomwalton78@googlemail.com>
2019-02-03 13:54:10 +0000

    Write 2 tests for cancelOrder function
    Write 2 failing tests for cancelling orders in the same queue,
    checking queue length after cancellation and that correct order
    was cancelled.
    
    Signed-off-by: Tom Walton
    Signed-off-by: Miaojun Huang

--- tests.js
+++ tests.js
@@ -19,7 +19,7 @@ let db;
 // Create IPFS instance
 const ipfs = new IPFS(ipfsOptions);
 
-
+// addOrder tests
 ipfs.on('ready', async () => {
   // Create OrbitDB instance
   const orbitdb = new OrbitDB(ipfs)
@@ -95,11 +95,72 @@ ipfs.on('ready', async () => {
 
   // Compute stats of tests that passed/failed
   if (num_tests_passed === num_tests_run) {
-    console.log("ALL " + num_tests_run + " TESTS PASSED!")
+    console.log("ALL " + num_tests_run + " addOrder TESTS PASSED!")
   } else {
-    console.log("NOT ALL TESTS PASSED!")
+    console.log("NOT ALL addOrder TESTS PASSED!")
     console.log(num_tests_passed + " out of " + num_tests_run + " tests passed.")
+  }
 
+  db.close();
+
+})
+
+// cancelOrder tests
+ipfs.on('ready', async () => {
+
+  // Create OrbitDB instance
+  const orbitdb = new OrbitDB(ipfs)
+
+  db = await orbitdb.keyvalue('test-database-2')
+
+  await db.put("metadata", {
+    best_bid: 1,
+    best_ask: 1,
+    tick_size: 0.01,
+  })
+
+  let ts_a = await addOrder(db, true, 100.0, 20, "A");
+  let ts_b = await addOrder(db, true, 50.0, 20, "B");
+
+  // Test cancelling orders within same queue, queue length changes
+  num_tests_run++;
+  try {
+    cancelOrder(db, 20, "A", ts_a);
+    assert.strictEqual(db.get(20).length, 1);
+    num_tests_passed++;
+  } catch (err) {
+    num_tests_failed++;
+    console.log(err.name, ": ", err.actual, err.operator, err.expected);
+  }
+
+  let ts_c = await addOrder(db, true, 100.0, 20, "C");
+
+  // Test cancelling orders within same queue, correct order gets cancelled
+  num_tests_run++;
+  try {
+    cancelOrder(db, 20, "B", ts_b);
+    assert.strictEqual(db.get(20)[0].user, "C");
+    num_tests_passed++;
+  } catch (err) {
+    num_tests_failed++;
+    console.log(err.name, ": ", err.actual, err.operator, err.expected);
+  }
+
+  // Test cancel last order in queue, queue gets deleted
+
+  // Test best bid changes when deleting best bid order
+
+  // Test best ask changes when deleting best ask order
+
+
+
+
+  // Compute stats of tests that passed/failed
+  if (num_tests_passed === num_tests_run) {
+    console.log("ALL " + num_tests_run + " cancelOrder TESTS PASSED!")
+  } else {
+    console.log("NOT ALL cancelOrder TESTS PASSED!")
+    console.log(num_tests_passed + " out of " + num_tests_run + " tests passed.")
   }
 
   db.close();
