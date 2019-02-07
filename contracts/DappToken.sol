2ca7cfad3c6d61d9d90054506ba4699e192b8ab3
C <john@ic.ac.uk>
2019-02-07 00:54:54 +0000

    2. Token Transfer Function

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,11 +1,89 @@
 pragma solidity ^0.5.0;
 
+// ERC20 standard requires six main functions:
+// totalSupply
+// balanceOf
+// allowance
+// transfer
+// approve
+// transferFrom
+
+// and two events:
+// event Transfer 
+// event Approval
+
 contract DappToken {
 
+	// token name
+	string public name = "Haha Token";
+
+	// token symbol
+	string public symbol = "HAHA";
+
+	//token version
+	string public version = "HAHA v1.0";
+
+	//total supply
 	uint256 public totalSupply;
 
-	constructor() public {
-		totalSupply = 1000000;
+	//Transfer event
+	//users can subscribe/listen to events, receive alerts if event is triggered
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+	);
 
+	//mapping should indicate which addresses hold what balances of tokens
+	mapping(address => uint256) public balanceOf;
+
+	constructor(uint256 _initialSupply) public {
+		//msg global variable has sender method
+		//for now, balance should be first address in ganache
+		balanceOf[msg.sender] = _initialSupply;
+		totalSupply = _initialSupply;
 	}
+
+	//transfer function
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		//must throw exception if msg.sender has insufficient balance
+		//if require evalutes to true, function continues to execute, if not then function execution stops
+		require(balanceOf[msg.sender] >= _value);
+
+		//transfer balance, decrease balance by amount transferred
+		balanceOf[msg.sender] -= _value;
+		//increase balance of recipient address
+		balanceOf[_to] += _value;
+
+		//transfer event must be triggered
+		emit Transfer(msg.sender, _to, _value);
+
+		//returns a boolean true if transfer successful
+		return true;
+	}
+
+
+
+
+
+	//transfer event
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,12 +1,95 @@
 var DappToken = artifacts.require("./DappToken.sol");
 
 contract('DappToken', function(accounts) {
-	it('sets the total supply on deployment', function() {
+	var tokenInstance;
+
+	it('initialises contract with correct values', function() {
+		return DappToken.deployed().then(function(instance) {
+			tokenInstance = instance;
+			return tokenInstance.name();
+		}).then(function(name) {
+			assert.equal(name, 'Haha Token', 'token initialised with correct name');
+			return tokenInstance.symbol();
+		}).then(function(symbol) {
+			assert.equal(symbol, 'HAHA', 'token initialised with correct symbol');
+			return tokenInstance.version();
+		}).then(function(version) {
+			assert.equal(version, 'HAHA v1.0', 'token version correct');
+		});
+	});
+
+	it('allocates initial supply on deployment', function() {
 		return DappToken.deployed().then(function(instance) {
 			tokenInstance = instance;
 			return tokenInstance.totalSupply();
 		}).then(function(totalSupply) {
 			assert.equal(totalSupply.toNumber(), 1000000, 'sets total supply to 1,000,000');
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(adminBalance) {
+			assert.equal(adminBalance.toNumber(), 1000000, 'allocates initial supply to admin account address');
 		});
 	});
+
+	it('transfers tokens from sender to receiver, updates balances and triggers Transfer event', function() {
+		return DappToken.deployed().then(function(instance) {
+			tokenInstance = instance;
+			//test 'require' statement
+			//should throw error if sum transferred is larger than sender's balance
+			//note 'call' doesn't trigger a transaction, only an inspection
+			//'transfer' triggers transaction
+			return tokenInstance.transfer.call(accounts[1], 10000000000);
+		}).then(assert.fail).catch(function(error) {
+			assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
+			//transfer should return boolean value true
+			return tokenInstance.transfer.call(accounts[1], 100000, { from: accounts[0]} );
+		}).then(function(success) {
+			assert.equal(success, true, 'returns true');
+			//transfer 100,000 HAHA from sender to recipient
+			return tokenInstance.transfer(accounts[1], 100000, { from: accounts[0] });
+		}).then(function(receipt) {
+			assert.equal(receipt.logs.length, 1, 'triggers one event');
+			assert.equal(receipt.logs[0].event, 'Transfer', 'should be Transfer event');
+			assert.equal(receipt.logs[0].args._from, accounts[0], 'logs account tokens are transferred from');
+			assert.equal(receipt.logs[0].args._to, accounts[1], 'logs account tokens are transferred to');
+			assert.equal(receipt.logs[0].args._value, 100000, 'logs transfer amount');
+			return tokenInstance.balanceOf(accounts[1]);
+		}).then(function(balance) {
+			assert.equal(balance.toNumber(), 100000, 'adds amount to recipient\'s account');
+			return tokenInstance.balanceOf(accounts[0]);
+		}).then(function(balance) {
+			assert.equal(balance.toNumber(), 900000, 'deducts amount from sender\'s account');
+		});
+	});
+
+
+
+
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
