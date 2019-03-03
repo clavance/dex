1b95bec8f5db6385302a9018869745744725b28b
C <john@ic.ac.uk>
2019-03-03 03:34:40 +0000

    Added Exchange.sol with test for token transfer

--- smart_contracts/migrations/2_deploy_contracts.js
+++ smart_contracts/migrations/2_deploy_contracts.js
@@ -1,7 +1,13 @@
 var OrbitToken = artifacts.require("./OrbitToken.sol");
 var DexToken = artifacts.require("./DexToken.sol");
+var Exchange = artifacts.require("./Exchange.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(OrbitToken, 1000000);
   deployer.deploy(DexToken, 1000000);
+  deployer.deploy(OrbitToken, 1000000).then(function() {
+  	// pass addresses of both OrbitToken and DexToken contracts into Exchange constructor
+  	// pass price of ORB in DEX in constructor
+  	var orbitPrice = 100;
+  	return deployer.deploy(Exchange, OrbitToken.address, DexToken.address, orbitPrice);
+  });
 };
