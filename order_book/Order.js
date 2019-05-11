cc2d51b00bbe18a2463515b05d24e0d525c17367
tmw15 <tmw15@ic.ac.uk>
2019-05-11 15:17:14 +0100

    Add documentation for Order class

--- order_book/Order.js
+++ order_book/Order.js
@@ -1,6 +1,19 @@
 "use strict"
 
+/**
+ * The Order class represents an order object. This is any order that has been
+ * placed by a user (could be maker or taker), and could even be only part of
+ * an order (e.g. part that has matched).
+ */
 class Order {
+
+  /**
+   * @param {boolean} is_buy - true if order is a buy order, false if sell
+   * @param {number} amount - amount of currency to trade
+   * @param {number} price - price at which to trade currency
+   * @param {string} user - unique identifier for a user
+   * @param {number} timestamp - represents date and time, in milliseconds since 1 Jan 1970 00:00:00
+   */
   constructor(is_buy, amount, price, user, timestamp) {
     this.is_buy = is_buy;
     this.amount = amount;
