cf151f6827b0e3576898d0386b22bd905c50088b
tmw15 <tmw15@ic.ac.uk>
2019-05-11 16:02:08 +0100

    Add documentation for Trade class

--- order_book/Trade.js
+++ order_book/Trade.js
@@ -1,12 +1,14 @@
 "use strict"
 
+/**
+ * The Trade class represents an trade object. A trade object consists of a maker
+ * Order and a taker Order, as well as the timestamp at which the trade occurred.
+ */
 class Trade {
 
 	/**
-	 * Initialise object with maker and taker orders, which together
-	   define a trade.
-	 * @param {Order} maker_order - Maker order (resitng on book).
-	 * @param {Order} taker_order - IPFS instance to use.
+	 * @param {Order} maker_order - Maker order (adds liquidity, rests on book)
+	 * @param {Order} taker_order - Taker order (removes liquidity, matching against resting order)
 	 */
   constructor(maker_order, taker_order) {
   	this.maker_order = maker_order;
